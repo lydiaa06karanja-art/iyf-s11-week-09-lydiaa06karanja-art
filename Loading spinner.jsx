6ec0444128b127e78ffd85e0375749bab1240a92
Lydia karanja <lydiaa06karanja@gmail.com>
2026-08-14 19:46:30 +0300

    Create Loading spinner.jsx

---
+++ Loading spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LoadingSpinner({ size = 'medium', text = 'Loading...' }) {
+  return (
+    <div className={`loading-spinner ${size}`}>
+      <div className="spinner"></div>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default LoadingSpinner
