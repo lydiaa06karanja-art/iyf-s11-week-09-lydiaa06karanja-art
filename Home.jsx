1ad95ad2254ff2274a959c72135a0ad960bb1c37
Lydia karanja <lydiaa06karanja@gmail.com>
2026-08-15 21:33:43 +0300

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -5,11 +5,13 @@ import WindowSize from '../WindowSize'
 import ThemeToggle from '../ThemeToggle'
 import PostList from '../PostList'
 
+import Button from '../components/Button';
 function Home() {
   const [userId, setUserId] = useState(1)
 
   return (
-    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+<div className="p-5 font-sans">
+    
       <h1>Week 9 - Task 17.1: useEffect Hook</h1>
       <PostList />
       <section>
@@ -22,9 +24,18 @@ function Home() {
       <section>
         <h2>Exercise 2: Common useEffect Patterns</h2>
         <h3>Pattern 1: Fetch Data</h3>
-        <button onClick={() => setUserId(userId === 1 ? 2 : 1)}>
-          Switch User ID: {userId}
-        </button>
+
+        <Button 
+  variant="primary" 
+  onClick={() => setUserId(userId === 1 ? 2 : 1)}
+>
+  Switch User ID: {userId}
+</Button>
+
+        <div style={{ marginTop: '10px', display: 'flex', gap: '10px' }}>
+  <Button variant="secondary">Secondary</Button>
+  <Button variant="danger">Danger</Button>
+</div>
         <UserProfile userId={userId} />
 
         <h3>Pattern 2: Event Listeners</h3>
